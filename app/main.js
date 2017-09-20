573f971770e916447fd317df18b0d8bd56806f64
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-20 10:11:12 +0200

    add: webpack pour css

--- app/main.js
+++ app/main.js
@@ -2,6 +2,9 @@ import 'angular';
 import 'angular-ui-router';
 import './ors-layout/ors-layout.js';
 import './ors-route/ors-route.js';
+import 'angular/angular-csp.css';
+import './style.scss';
+
 
 const app = angular.module('main', ['ors-layout', 'ors-route']);
 console.log('coucou');
