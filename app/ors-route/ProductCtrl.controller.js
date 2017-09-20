76dcb7a8f37dbc037b3ab191299b97036eedbd06
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-20 13:23:22 +0200

    Add: Web Services

--- app/ors-route/ProductCtrl.controller.js
+++ app/ors-route/ProductCtrl.controller.js
@@ -1,14 +1,28 @@
 export class ProductCtrl {
-    constructor($http) {
+    constructor($http, $q) {
         this.$http = $http;
+        this.$q = $q;
     }
     start() {
         console.log('start');
         this.$http.get('../ws/s1').then(response => {
-            console.log('response', response);
+            console.log('s1', response.data);
+            return this.$q.all([
+                this.$http.get('../ws/s2').then(response => {
+                    console.log('s2', response.data);
+                    return this.$http.get('../ws/s5');
+                }),
+                this.$http.get('../ws/s3'),
+                this.$http.get('../ws/s4')
+            ]);
+        }).then(responses => {
+            console.log('s5 s3 s4', responses);
+            return this.$http.get('../ws/s6');
+        }).then(response => {
+            console.log('s6', response.data);
         }).catch(error => {
             console.log('error', error);
         });
     }
 }
-ProductCtrl.$inject = ['$http'];
+ProductCtrl.$inject = ['$http', '$q'];
