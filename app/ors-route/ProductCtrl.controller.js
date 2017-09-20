ddbbd00409212156e3ff4d5d7fccb82d0c614814
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-20 13:08:59 +0200

    add: appel s1

--- app/ors-route/ProductCtrl.controller.js
+++ app/ors-route/ProductCtrl.controller.js
@@ -1,7 +1,14 @@
 export class ProductCtrl {
-    constructor() { }
+    constructor($http) {
+        this.$http = $http;
+    }
     start() {
         console.log('start');
+        this.$http.get('../ws/s1').then(response => {
+            console.log('response', response);
+        }).catch(error => {
+            console.log('error', error);
+        });
     }
 }
-ProductCtrl.$inject = [];
+ProductCtrl.$inject = ['$http'];
